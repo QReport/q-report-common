770c42fbdb3b25f6b9eb9e29aa31ab2b242f51aa
RedEnergy <forwot163@gmail.com>
2016-01-28 16:08:36 +0400

    Added extension function to count tickets, count active users and count average response time

--- src/main/kotlin/ru/redenergy/report/common/entity/Ticket.kt
+++ src/main/kotlin/ru/redenergy/report/common/entity/Ticket.kt
@@ -30,3 +30,33 @@ data class Ticket(var uid: UUID = UUID.randomUUID(), var status: TicketStatus, v
     private constructor(): this(UUID.randomUUID(), TicketStatus.OPEN, "unknown", TicketReason.OTHER, arrayListOf())
 
 }
+
+/**
+ * Map in which key is TicketReason and value is amount of tickets in this category
+ */
+fun MutableList<Ticket>.countReasons(): Map<TicketReason, Int> =
+        mapOf(*TicketReason.values()
+            .map { r -> r to count { it.reason.equals(r) } }
+            .toTypedArray())
+
+/**
+ * Map with users who send most of tickets
+ * @param limiter - first N users to return
+ */
+fun MutableList<Ticket>.activeUsers(limiter: Int): Map<String, Int> =
+        linkedMapOf(*map { it.sender }
+                    .map { it to  count { t -> t.sender.equals(it) }}
+                    .toTypedArray())
+        .toList()
+                .sortedBy { it.second }
+                .reversed()
+                .run { slice(0..Math.min(count() - 1, limiter - 1)) }
+                .toMap()
+
+/**
+ * Average response time between first original sender's message and first response of administrator
+ */
+fun MutableList<Ticket>.averageResponseTime(): Long =
+        filter { t -> t.messages.any { m -> m.sender != t.sender } }
+        .map { it.messages.first { m -> m.sender != it.sender } .timestamp - it.messages[0].timestamp }
+        .run { return sum() / count() }
